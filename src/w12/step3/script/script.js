67eaf4cd0ef37fe13009aa786a2d49363af723f5
gusw01 <162656220+gusw01@users.noreply.github.com>
2024-05-23 12:03:52 +0900

    12주차 수업

---
+++ src/w12/step3/script/script.js
@@ -0,0 +1,20 @@
+let elemBody = document.body;
+elemBody.style.backgroundColor = "#0000ff";
+// let aCard = document.querySelector(".card");
+// aCard.addEventListener("click", function () {
+//   console.log("클릭!");
+//   aCard.classList.toggle("card--state-clicked");
+//   console.log(aCard.classList);
+// });
+let cards = document.querySelectorAll(".card");
+console.log(cards);
+// cards.forEach((eachCard) => {
+//   eachCard.addEventListener("click, funtion () {}");
+// });
+
+for (let i = 0; i < cards.length; i++) {
+  let eachCard = cards[i];
+  eachCard.addEventListener("click", function () {
+    eachCard.classList.toggle("card--state-clicked");
+  });
+}
